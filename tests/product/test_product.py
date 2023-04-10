9be69d7a26effe14a3a3de8d74b4d71ab3aabaa4
Elienay Alves <elienay8486@gmail.com>
2023-04-10 16:46:41 -0300

    Requisito 1 again

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -2,11 +2,13 @@ from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
+
     product = Product(
         1,
         "Fried Chicken",
         "Two Sons Company",
         "1990-12-31",
+        "2020-12-31",
         "123456789",
         "Maintain in your stomach",
     )
@@ -14,5 +16,6 @@ def test_cria_produto():
     assert product.nome_do_produto == "Fried Chicken"
     assert product.nome_da_empresa == "Two Sons Company"
     assert product.data_de_fabricacao == "1990-12-31"
+    assert product.data_de_validade == "2020-12-31"
     assert product.numero_de_serie == "123456789"
     assert product.instrucoes_de_armazenamento == "Maintain in your stomach"
