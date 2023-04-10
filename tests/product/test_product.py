829be76735eed07913f00a8ed7e4ffa3ae641d39
Elienay Alves <elienay8486@gmail.com>
2023-04-10 16:15:01 -0300

    Iniciando Projeto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,18 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Fried Chicken",
+        "Two Sons Company",
+        "1990-12-31",
+        "123456789",
+        "Maintain in your stomach",
+    )
+    assert product.id == 1
+    assert product.nome_do_produto == "Fried Chicken"
+    assert product.nome_da_empresa == "Two Sons Company"
+    assert product.data_de_fabricacao == "1990-12-31"
+    assert product.numero_de_serie == "123456789"
+    assert product.instrucoes_de_armazenamento == "Maintain in your stomach"
